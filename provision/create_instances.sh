ef729893003bb885b5ee00bdf685d536fb513379
John Matthews <jwmatthews@gmail.com>
2013-08-14 17:14:32 -0400

    Experimenting with ansible groups and tagging launched instances

---
+++ provision/create_instances.sh
@@ -0,0 +1,12 @@
+export ANSIBLE_HOSTS=~/ansible_hosts
+export ANSIBLE_HOST_KEY_CHECKING=False
+export SSH_PRIV_KEY=~/.ssh/splice_rsa
+export LOG_DIR=./logs
+
+grep "localhost" $ANSIBLE_HOSTS &> /dev/null || echo "localhost" > ~/ansible_hosts
+
+if [ ! -d ${LOG_DIR} ]; then
+  mkdir ${LOG_DIR}
+fi
+
+time ansible-playbook rhui_dev_create_instances.yml -vv --private-key=${SSH_PRIV_KEY} | tee ${LOG_DIR}/rhui_dev_create_instances.log
