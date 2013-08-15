dbe226282c0e108689c4b8e7f11d5dcedee2a96b
John Matthews <jwmatthews@gmail.com>
2013-08-15 12:57:34 -0400

    Instance now uses a LVM for the filesystem, epel repo is installed and git checkout of pulp is performed.

--- provision/simple.sh
+++ provision/simple.sh
@@ -1,12 +1,12 @@
-export ANSIBLE_HOSTS=~/ansible_hosts
+export RHUI_HOSTS_FILE=./rhui_hosts
 export ANSIBLE_HOST_KEY_CHECKING=False
 export SSH_PRIV_KEY=~/.ssh/splice_rsa
 export LOG_DIR=./logs
 
-grep "localhost" $ANSIBLE_HOSTS &> /dev/null || echo "localhost" > ~/ansible_hosts
+grep "localhost" $RHUI_HOSTS_FILE &> /dev/null || echo "localhost" > ${RHUI_HOSTS_FILE}
 
 if [ ! -d ${LOG_DIR} ]; then
   mkdir ${LOG_DIR}
 fi
 
-time ansible-playbook simple.yml -vv --private-key=${SSH_PRIV_KEY} | tee ${LOG_DIR}/simple.log
+time ansible-playbook simple.yml -i ${RHUI_HOSTS_FILE} -vv --private-key=${SSH_PRIV_KEY} | tee ${LOG_DIR}/simple.log
