89a9334472c9ae669177baeb3fc0399f52374361
John Matthews <jwmatthews@gmail.com>
2013-11-11 17:32:15 -0500

    Fixed bad path

--- install/gen_certs.sh
+++ install/gen_certs.sh
@@ -8,5 +8,5 @@ echo "Will generate certs for: "
 echo "  RHUA   = ${RHUA}"
 echo "  CDS_01 = ${CDS_01}"
 echo "  CDS_02 = ${CDS_02}"
-/usr/share/rh-rhua/create_rhui_ssl_certs.sh --noencrypt ${RHUA} ${CDS_01} ${CDS_02}
+/usr/share/rh-rhua/rhui_certs/create_rhui_ssl_certs.sh --noencrypt ${RHUA} ${CDS_01} ${CDS_02}
 
