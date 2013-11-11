5769474bd21d6deae2f14684bcfb0274e2338789
John Matthews <jwmatthews@gmail.com>
2013-11-11 16:58:11 -0500

    Update to account for --noencrypt argument for certificate generation

--- install/gen_certs.sh
+++ install/gen_certs.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
-
 source ./hostnames
 source ./vars
 
-cp -R /usr/share/rh-rhua/rhui_certs /home/ec2-user
-cd /home/ec2-user/rhui_certs
+mkdir ~/rhui_certs
+cd ~/rhui_certs
 echo "Will generate certs for: "
 echo "  RHUA   = ${RHUA}"
 echo "  CDS_01 = ${CDS_01}"
 echo "  CDS_02 = ${CDS_02}"
-ENCRYPT_KEYS=0 ./create_rhui_ssl_certs.sh ${RHUA} ${CDS_01} ${CDS_02}
+/usr/share/rh-rhua/create_rhui_ssl_certs.sh --noencrypt ${RHUA} ${CDS_01} ${CDS_02}
 
