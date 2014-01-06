5177ce4ff552cf41f037424c2a82b41b894aa126
John Matthews <jwmatthews@gmail.com>
2014-01-06 18:11:48 -0500

    Tweaks to get pulp v2 deployed and adjust client config to match hostname

--- pulp/adjust_pulp_config.sh
+++ pulp/adjust_pulp_config.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 HOSTNAME=`hostname`
-#sed "s/localhost/${HOSTNAME}/" -i /etc/pulp/server.conf
-sed "s/host = localhost.localdomain/host\ =\ ${HOSTNAME}/" -i /etc/pulp/server.conf
+sed "s/host = localhost.localdomain/host\ =\ ${HOSTNAME}/" -i /etc/pulp/admin/admin.conf
