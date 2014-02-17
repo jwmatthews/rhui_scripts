90d20a83c092e654ec9f30b97a6eedcaf0943998
David Gao <dgao@redhat.com>
2014-02-17 10:26:21 -0500

    added jbeap6 repos

--- rhui_provision/files/setup/remote_host/custom_repos_jboss.sh
+++ rhui_provision/files/setup/remote_host/custom_repos_jboss.sh
@@ -34,6 +34,38 @@ EOQ
 cat <<EOQ | rhui-manager --username admin --password admin
 r
 c
+rhui-client-config-rhel-server-6-i386-jbeap-6
+Red Hat Update Infrastructure 2.0 Client Configuration Server 6 i386 JBoss EAP 6
+rhui-client-config/rhel/server/6/i386/jbeap/6
+2
+y
+rhui-client-config/rhel/server/6/i386/jbeap/6
+y
+n
+n
+y
+q
+EOQ
+
+cat <<EOQ | rhui-manager --username admin --password admin
+r
+c
+rhui-client-config-rhel-server-6-x86_64-jbeap-6
+Red Hat Update Infrastructure 2.0 Client Configuration Server 6 x86_64 JBoss EAP 6
+rhui-client-config/rhel/server/6/x86_64/jbeap/6
+2
+y
+rhui-client-config/rhel/server/6/x86_64/jbeap/6
+y
+n
+n
+y
+q
+EOQ
+
+cat <<EOQ | rhui-manager --username admin --password admin
+r
+c
 rhui-client-config-rhel-server-6-i386-jbews-1
 Red Hat Update Infrastructure 2.0 Client Configuration Server 6 i386 JBoss EWS 1
 rhui-client-config/rhel/server/6/i386/jbews/1
