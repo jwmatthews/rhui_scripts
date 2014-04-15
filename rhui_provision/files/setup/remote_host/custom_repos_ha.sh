fd29c4d8622206f4d7d71bf0a5aaea4b2fd4b4f5
dgao <dgao@yongqi.usersys.redhat.com>
2014-04-15 11:37:40 -0400

    Created rhel6 HA custom repo and rhel6 VSA custom repo

---
+++ rhui_provision/files/setup/remote_host/custom_repos_ha.sh
@@ -0,0 +1,15 @@
+cat <<EOQ | rhui-manager --username admin --password admin
+r
+c
+rhui-client-config-server-6-ha
+Red Hat Update Infrastructure 2.0 Client Configuration Server 6 High Availability 
+rhui-client-config/rhel/server/6/x86_64/ha/
+2
+y
+rhui-client-config/rhel/server/6/\$basearch/ha/1.0
+y
+n
+n
+y
+q
+EOQ
---
+++ rhui_provision/files/setup/remote_host/custom_repos_vsa.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+cat <<EOQ | rhui-manager --username admin --password admin
+r
+c
+rhui-client-config-rhel-server-6-x86_64-vsa
+Red Hat Update Infrastructure 2.0 Client Configuration Server 6 VSA 1.0
+rhui-client-config/rhel/server/6/x86_64/vsa/1.0
+2
+y
+rhui-client-config/rhel/server/6/\$basearch/vsa/1.0
+y
+n
+n
+y
+q
+EOQ
+
+cat <<EOQ | rhui-manager --username admin --password admin
+r
+c
+rhui-client-config-rhel-beta-server-6-vsa2
+Red Hat Update Infrastructure 2.0 Client Configuration Server 6 Beta VSA 2.0
+rhui-client-config/beta/rhel/server/6/x86_64/vsa/2.0
+2
+y
+rhui-client-config/beta/rhel/server/6/\$basearch/vsa/2.0
+y
+n
+n
+y
+q
+EOQ
