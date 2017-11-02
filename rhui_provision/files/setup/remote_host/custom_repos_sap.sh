de5d9bcaad116cea3187a0ec7f0671d211995dbe
Derek Whatley <dwhatley@redhat.com>
2017-11-02 16:38:12 -0400

    Add a custom repo for Server 7 SAP HANA client RPM.

--- rhui_provision/files/setup/remote_host/custom_repos_sap.sh
+++ rhui_provision/files/setup/remote_host/custom_repos_sap.sh
@@ -30,3 +30,19 @@ n
 y
 q
 EOQ
+
+cat <<EOQ | rhui-manager --username admin --password admin
+r
+c
+rhui-client-config-rhel-server-7-x86_64-sap-hana
+Red Hat Update Infrastructure 2.0 Client Configuration Server 7 SAP Hana x86_64
+rhui-client-config/rhel/server/7/x86_64/sap-hana/
+2
+y
+rhui-client-config/rhel/server/7/\$basearch/sap-hana
+y
+n
+n
+y
+q
+EOQ
